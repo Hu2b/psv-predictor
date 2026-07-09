856bcf15280001454bde7561283b4d3ae34af13f
Hu2b <76754652+Hu2b@users.noreply.github.com>
2026-07-09 18:24:30 +0200

    Delete api/clearcache.js

--- api/clearcache.js
+++
@@ -1,8 +0,0 @@
-import { kvDel } from './_kv.js'
-
-export default async function handler(req, res) {
-  await kvDel('psv:fixtures:fd')
-  await kvDel('psv:fixtures:fd:2025')
-  await kvDel('psv:fixtures:fd:2026')
-  res.status(200).json({ cleared: true })
-}
